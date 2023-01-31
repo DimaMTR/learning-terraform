16f5bbb8ab2ecad8f898eb0d8773fa37e4138dbd
Dmitry <dgm1198@gmail.com>
2023-01-31 13:02:21 -0800

    Changing instance to GCP Compute engine

--- main.tf
+++ main.tf
@@ -1,24 +1,43 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+# data "aws_ami" "app_ami" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#   filter {
+#     name   = "name"
+#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["979382823631"] # Bitnami
+# }
+
+# resource "aws_instance" "web" {
+#   ami           = data.aws_ami.app_ami.id
+#   instance_type = "t3.nano"
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+resource "google_compute_instance" "default" {
+  name         = "test-vm"
+  machine_type = "e2-medium"
+
+  tags = ["foo", "HelloWorld"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
+  network_interface {
+    network = "default"
   }
 }
