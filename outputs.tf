11d7efff225e99ca67e8af9ab08a955fb9d8eac0
Dmitry <dgm1198@gmail.com>
2023-01-31 20:23:26 -0800

    changing networking rules and machine

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_name" {
- value = google_compute_instance.test-vm.name
+ value = google_compute_instance.nodejs-server.name
 }
 
 output "instance_id" {
- value = google_compute_instance.test-vm.instance_id
+ value = google_compute_instance.nodejs-server.instance_id
 }
