3a64b43964bc7abf4d4b8eecc75a220566cae1d4
Dmitry <dgm1198@gmail.com>
2023-01-31 20:21:54 -0800

    changing networking rules and machine

--- main.tf
+++ main.tf
@@ -23,7 +23,46 @@
 #   }
 # }
 
-resource "google_compute_instance" "test-vm" {
+
+data "google_compute_network" "default" {
+  name = "default"
+}
+
+resource "google_compute_firewall" "nodejs-server"{
+  name        = "nodejs-server"
+  description = "Allow to access VM from public IPs"
+  
+  network     = data.google_compute_network.default.name
+
+  rules = [
+    google_compute_firewall_policy_rule.nodejs-http-in.self_link,
+    google_compute_firewall_policy_rule.nodejs-http-out.self_link,
+  ]
+}
+
+resource "google_compute_firewall_policy_rule" "nodejs-http-in" {
+  name = "nodejs-http-in"
+  action = "ALLOW"
+  direction = "INGRESS"
+
+  match {
+    versioned_expr = "true"
+  }
+
+}
+
+resource "google_compute_firewall_policy_rule" "nodejs-http-out" {
+  name = "nodejs-http-out"
+  action = "ALLOW"
+
+  match {
+    versioned_expr = "true"
+  }
+
+  direction = "EGRESS"
+}
+
+resource "google_compute_instance" "nodejs-server" {
   name         = var.instance_name
   machine_type = var.instance_type
   zone         = "us-west1-b"
@@ -32,13 +71,29 @@ resource "google_compute_instance" "test-vm" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        os = "debian"
-      }
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
+
+  connection {
+    type        = "ssh"
+    host        = self.network_interface.0.access_config.0.nat_ip
+    user        = "root"
+    private_key = file("~/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nodejs",
+      "sudo apt-get install -y npm",
+      "sudo npm install -g express",
+      "echo 'const express = require(\"express\");\nconst app = express();\napp.get(\"/\", (req, res) => {\n res.send(\"Hello from Node.js!\");\n});\napp.listen(3000, () => {\n console.log(\"Node.js server is listening on port 3000\");\n});' > app.js",
+      "sudo node app.js &"
+    ]
+  }
+
   network_interface {
-    network = "default"
+    network = data.google_compute_network.default.name
   }
 }
